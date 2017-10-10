bfdb4857063c66da94b15aacf2c2b0337d27e9eb
Piotrek Kiełek <Piotrek@Piotreks-MacBook-Pro.local>
2017-10-10 21:48:08 +0200

    Stworzyłem projekt w Xcode

---
+++ Charm-them-all/Charm-them-all/main.cpp
@@ -0,0 +1,64 @@
+
+//
+// Disclamer:
+// ----------
+//
+// This code will work only if you selected window, graphics and audio.
+//
+// Note that the "Run Script" build phase will copy the required frameworks
+// or dylibs to your application bundle so you can execute it on any OS X
+// computer.
+//
+// Your resource files (images, sounds, fonts, ...) are also copied to your
+// application bundle. To get the path to these resource, use the helper
+// method resourcePath() from ResourcePath.hpp
+//
+
+#include <SFML/Graphics.hpp>
+
+// Here is a small helper for you ! Have a look.
+#include "ResourcePath.hpp"
+
+int main(int, char const**)
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML window");
+
+    // Create a graphical text to display
+    sf::Font font;
+    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
+        return EXIT_FAILURE;
+    }
+    sf::Text text("Pozdrawiam mame", font, 100);
+    text.setColor(sf::Color::Black);
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+            // Escape pressed: exit
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
+        }
+
+        // Clear screen
+        window.clear(sf::Color::White);
+
+        // Draw the string
+        window.draw(text);
+
+        // Update the window
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
